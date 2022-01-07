b7fda3f0792e684ef877e89a08278da80c19d25c
Josevillarr <jsuperman5@hotmail.com>
2022-01-07 00:19:48 -0800

    page clock

--- assets/README.md
+++ assets/README.md
@@ -2,4 +2,6 @@
 
 * Color coded daily planner.
 
+* As the hours pass and the events pass current time, the color changes.
+
 * Information gets saved into local storage to persist a refresh.
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
-var test = function() {
-    console.log("Test 123");
+var caliTime = document.querySelector("#time-cali");
+var currentTime = moment();
+
+
+var instaUpdate = function () {
+    var now = moment();
+    var timeFormat = now.format("MMMM Do YYYY, h:mm:ss A")
+    
+    caliTime.textContent = timeFormat;
 }
 
-test();
+setInterval(instaUpdate, 1000);
+
+instaUpdate();
+
