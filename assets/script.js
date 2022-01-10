e0ea710a256ee19fefc78af6f3ab0ded48f0c7a8
Josevillarr <jsuperman5@hotmail.com>
2022-01-09 20:08:54 -0800

    input box

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
+var eventButton = document.getElementById("event-button");
+var eventInfo = document.getElementById("event-info");
+
+
 var caliTime = document.querySelector("#time-cali");
 var currentTime = moment();
-
 
 var instaUpdate = function () {
     var now = moment();
@@ -9,7 +12,15 @@ var instaUpdate = function () {
     caliTime.textContent = timeFormat;
 }
 
+function formShow() {
+    eventButton.classList.add("hide");
+    eventInfo.classList.remove("hide");
+
+}
+
+
 setInterval(instaUpdate, 1000);
 
 instaUpdate();
 
+eventButton.addEventListener("click", formShow)
